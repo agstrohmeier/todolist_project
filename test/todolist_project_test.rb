45aec7f651369cb43e29120dc63349df233eb468
Andrew Strohmeier <andrewstrohmeier@andrews-mini.lan>
2023-02-22 08:49:43 -0700

    Added gemfile, installed dependencies using bundler, added bundler to the project and test project files.

--- test/todolist_project_test.rb
+++ test/todolist_project_test.rb
@@ -1,9 +1,10 @@
+require 'bundler/setup'
 require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
-require_relative '..lib/todolist_project'
-#test_edit
+require_relative '../lib/todolist_project'
+
 class TodoListTest < MiniTest::Test
   def setup
     @todo1 = Todo.new('Buy milk')
